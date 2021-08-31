a0e30ebc18f3afcda78792b6969fac3ac2969efd
Patpatty19 <89431589+Patpatty19@users.noreply.github.com>
2021-08-31 16:41:45 +0800

    Add files via upload

---
+++ main3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+int number1, number2;
+
+cout << "Enter first number: ";
+cin >> number1;
+cout << endl << "End second number: ";
+cin >> number2;
+
+int sum = number1 + number2;
+cout << endl << "Sum of the two numbers is: " << sum;
+
+int diff = number1 - number2;
+cout << endl << "Difference of the two numbers is: " << diff;
+
+int prod = number1 * number2;
+cout << endl << "Product of the two numbers is: " << prod; 
+
+int quot = number1 / number2;
+cout << endl << "Quotient of the two numbers is: " << quot;
+
+	
+}
